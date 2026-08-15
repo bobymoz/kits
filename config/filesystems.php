3fe1b374837c1e1202f65dd15daaf8b5350e0b53
bobymoz <itanhagira@gmail.com>
2026-08-16 00:28:26 +0200

    Corrigindo sintaxe do arquivo filesystems e limpando excessos

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,10 +1,8 @@
- env('FILESYSTEM_DISK', 'local'),
+<?php
 
-    /*
-    |--------------------------------------------------------------------------
-    | Filesystem Disks
-    |--------------------------------------------------------------------------
-    */
+return [
+
+    'default' => env('FILESYSTEM_DISK', 'local'),
 
     'disks' => [
 
@@ -26,25 +24,7 @@
             'throw' => false,
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-        ],
-
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Symbolic Links
-    |--------------------------------------------------------------------------
-    */
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
